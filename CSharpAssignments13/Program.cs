d05c9b4efbcbfb04932366015f37cc519e8ed289
roxanamiron <32548532+roxanamiron@users.noreply.github.com>
2019-01-04 14:26:59 +0200

    Commit changes

---
+++ CSharpAssignments13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignments13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //13.Create a program to create an HTML file containing the list of images (PNG and JPG) in the current directory.
+            //For example, in the current directory there are images called.
+            //1.png
+            //2.jpg
+            //The result should be:
+            //< html >
+            //< body >
+            //< p > 1.png </ p >
+            //< img src = "1.png" alt = "1.png" />
+            //   < p > 2.jpg </ p >
+            //   < img src = "2.jpg" alt = "2.jpg" />
+            //      </ body >
+            //      </ html >
+
+        }
+    }
+}
