2c4d053e3814403929ccc2bde359d72a0ac210e0
roxanamiron <32548532+roxanamiron@users.noreply.github.com>
2019-01-21 17:25:15 +0200

    Prog6

---
+++ Programing1/Programing 6/Loop.cs
@@ -0,0 +1,226 @@
+﻿using System;
+
+namespace Programing_6
+{
+    public class Loop
+    {
+       public void Method()
+        {
+            int counter = 0;
+            // Execute the loop body while the loop condition holds
+            while (counter <= 9)
+            {
+                // Print the counter value
+                Console.WriteLine("Number : " + counter);
+                // Increment the counter
+                counter++;
+            }
+        }
+
+      
+
+        public void Sum()
+        {
+            //find the sum of the numbers from 1 to n.
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            int sum = 1;
+            Console.Write("The sum 1");
+            while (num < n)
+            {
+                num++;
+                sum += num;
+                Console.Write(" + " + num);
+            }
+            Console.WriteLine(" = " + sum);
+        }
+        public void CheckPrime()
+        {
+            Console.Write("Enter a positive number: ");
+            int num = int.Parse(Console.ReadLine());
+            int divider = 2;
+            int maxDivider = (int)Math.Sqrt(num);
+            bool prime = true;
+            while (prime && (divider <= maxDivider))
+            {
+                if (num % divider == 0)
+                {
+                    prime = false;
+                }
+                divider++;
+            }
+            Console.WriteLine("Prime? " + prime);
+        }
+        public void CalculateFactorial()
+        {
+            int n = int.Parse(Console.ReadLine());
+            // "decimal" is the biggest C# type that can hold integer values
+            decimal factorial = 1;
+            // Perform an "infinite loop"
+            while (true)
+            {
+                if (n <= 1)
+                {
+                    break;
+                }
+                factorial *= n;
+                n--;
+            }
+            Console.WriteLine("n! = " + factorial);
+        }
+        public void CalculateFactorial2()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            decimal factorial = 1;
+            do
+            {
+                factorial *= n;
+                n--;
+            } while (n > 0);
+            Console.WriteLine("n! = " + factorial);
+        }
+      public void FindTheProduct()
+        {
+            //find the product of all numbers in the range [n…m]. .
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("m = ");
+            int m = int.Parse(Console.ReadLine());
+
+            int num = n;
+            long product = 1;
+            do
+            {
+                product *= num;
+                num++;
+            } while (num <= m);
+
+            Console.WriteLine("product[n...m] = " + product);
+        }
+        public void RaiseTheNumber()
+        {
+            //raises the number n to a power of m
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("m = ");
+            int m = int.Parse(Console.ReadLine());
+            decimal result = 1;
+            for (int i = 0; i < m; i++)
+            {
+                result *= n;
+            }
+            Console.WriteLine("n^m = " + result);
+        }
+        public void Counter()
+        {
+            for (int small = 1, large = 10; small < large; small++, large--)
+            {
+                Console.WriteLine(small + " " + large);
+            }
+        }
+        public void SummOfAllOdd()
+        {
+            //calculate the sum of all odd integers in the range [1…n], which are not divisible by 7 
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i += 2)
+            {
+                if (i % 7 == 0)
+                {
+                    continue;
+                }
+                sum += i;
+            }
+            Console.WriteLine("sum = " + sum);
+        }
+        public void PrintTriangle()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(col + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void PrintPrime()
+        {
+            //prime number in the interval [n…m].
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("m = ");
+            int m = int.Parse(Console.ReadLine());
+
+            for (int num = n; num <= m; num++)
+            {
+                bool prime = true;
+                int divider = 2;
+                int maxDivider = (int)Math.Sqrt(num);
+                while (divider <= maxDivider)
+                {
+                    if (num % divider == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                    divider++;
+                }
+                if (prime)
+                {
+                    Console.Write(" " + num);
+                }
+            }
+        }
+        public void CheckLuckyNumbers()
+        {
+            // find and print all four-digit numbers of the type ABCD, where: A+B = C+D (we call them lucky numbers).
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 0; b <= 9; b++)
+                {
+                    for (int c = 0; c <= 9; c++)
+                    {
+                        for (int d = 0; d <= 9; d++)
+                        {
+                            if ((a + b) == (c + d))
+                            {
+                                Console.WriteLine(
+                                  " " + a + " " + b + " " + c + " " + d);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void FindCombinations()
+        {
+            //find all possible combinations of the lottery game "6/49". We have to find and print all possible extracts of 6 different numbers, each of which is in the range [1…49].
+            for (int i1 = 1; i1 <= 44; i1++)
+            {
+                for (int i2 = i1 + 1; i2 <= 45; i2++)
+                {
+                    for (int i3 = i2 + 1; i3 <= 46; i3++)
+                    {
+                        for (int i4 = i3 + 1; i4 <= 47; i4++)
+                        {
+                            for (int i5 = i4 + 1; i5 <= 48; i5++)
+                            {
+                                for (int i6 = i5 + 1; i6 <= 49; i6++)
+                                {
+                                    Console.WriteLine(i1 + " " + i2 + " " +
+                                         i3 + " " + i4 + " " + i5 + " " + i6);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programing1/Programing 6/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programing_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+            //PrintNumbers();
+            //2.
+            // PrintNotDivizibile();
+            //3.
+            // PrintSmallestAndGreatest();
+            //4.
+            //PrintPossibilities();
+            //5.
+            //PrintSum();
+            //6.
+            // CalculateFactorial();
+            //7.
+            // CalculateFactorial2();
+            //8.
+            // CalculateCatalanNumbers();
+            //9.
+            //CalculateTheSum();
+            //10.
+            PrintMatrix();
+            Console.ReadLine();
+        }
+        static void PrintNumbers()
+        {
+            Console.WriteLine("\nWrite a program that prints on the console the numbers from 1 to N. The number N should be read from the standard input.");
+
+            Console.WriteLine("Input n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+        static void PrintNotDivizibile()
+        {
+            Console.WriteLine("Write a program that prints on the console the numbers from 1 to N, " +
+                "which are not divisible by 3 and 7 simultaneously. The number N should be read from the standard input.");
+
+            Console.WriteLine("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+
+            {
+                if (i % (3 * 7) != 0)
+                    Console.WriteLine(i);
+            }
+        }
+        static void PrintSmallestAndGreatest()
+        {
+            Console.WriteLine("\nWrite a program that reads from the console a series of integers and prints the smallest and largest of them.");
+            Console.WriteLine("Counter = ");
+            int counter = int.Parse(Console.ReadLine());
+            int smallest = 0; 
+            int largest = 0;
+
+            for(int i = 0; i <= counter; i++)
+            {
+                Console.WriteLine("Input number = ");
+                int number = int.Parse(Console.ReadLine());
+                if(i == 0)
+                {
+                    smallest = largest = number;
+                }
+                else
+                {
+                    if(smallest > number)
+                    {
+                        smallest = number;
+                    }
+                    if(largest < number)
+                    {
+                        largest = number;
+                    }
+                }
+                Console.WriteLine("Smalest {0}, Largest {1}",smallest,largest);
+            }
+        }
+        static void PrintPossibilities()
+        {
+            Console.WriteLine("\nWrite a program that prints all possible cards from a standard deck of cards, "+
+                "without jokers (there are 52 cards: 4 suits of 13 cards).");
+
+            //deci avem 52 de carti intr-un pachet de carti : numaratoarea incepe de la {1...14}
+            //si avem 4 seturi de culori : inima - 1, romb = 2, trefla = 3, frunza = 4
+
+            for(int cards = 2; cards <= 14; cards ++ )
+            {
+                for(int suits = 1; suits <= 4; suits ++)
+                {
+                    Console.WriteLine("Card {0}, Suits {1}", cards, suits);
+                }
+            }
+        }
+        static void PrintSum()
+        {
+            Console.WriteLine("\n Write a program that reads from the console number N and print the sum "+
+                "of the first N members of the Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, …");
+
+            Console.WriteLine("n = " );
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+            int firstNr = 1;
+            int secondNr = 1;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                result = firstNr + secondNr;
+                firstNr = secondNr;
+                secondNr = result;
+                sum += result;
+                Console.WriteLine("Sum = {0}",sum);
+            }
+
+
+        }
+        static void CalculateFactorial()
+        {
+            Console.WriteLine("\nWrite a program that calculates N!/K! for given N and K (1<K<N).");
+
+            Console.WriteLine("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("k = ");
+            int k = int.Parse(Console.ReadLine());
+            int factorial1 = 1;
+            int factorial2 = 1;
+
+            for (int i = n; n > 1; n --)
+            {
+                if(k > 1)
+                {
+                    factorial1 *= k;
+                    k--;
+                }
+               
+                factorial2 *= n;
+                Console.WriteLine("N! = {0}, K! = {1}", factorial2, factorial1);
+                var result = factorial2/factorial1;
+                Console.WriteLine("N!/K! = {0}", result);
+            }
+        }
+        static void CalculateFactorial2()
+        {
+            Console.WriteLine("\nWrite a program that calculates N!*K!/(N-K)! for given N and K (1<K<N).");
+
+            Console.WriteLine("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("k = ");
+            int k = int.Parse(Console.ReadLine());
+            int factorial1 = 1;
+            int factorial2 = 1;
+            int factorial3 = 1;
+
+            for (int i = n; n > 1; n--)
+            {
+                if (k > 1)
+                {
+                    factorial1 *= k;
+                    k--;
+                }
+
+                factorial2 *= n;
+                Console.WriteLine("N! = {0}, K! = {1}", factorial2, factorial1);
+                factorial3 *= (n - k);
+                var result = (factorial2 * factorial1) / factorial3;
+                Console.WriteLine("N!*K!/(N-K)! = {0}", result);
+            }
+        }
+        static void CalculateCatalanNumbers()
+        {
+            Console.WriteLine("\nIn combinatorics, the Catalan numbers are calculated by the following formula: ..."+
+                ", for n ≥ 0. Write a program that calculates the nth Catalan number by given n.");
+
+            Console.WriteLine("n = ");
+            int n = int.Parse(Console.ReadLine());
+           int factorial1 = n + 1;
+          int factorial2 = 2 * n;
+
+
+           for(int i = factorial2; i > 0; i--)
+            {
+                factorial2 *= i;
+            }
+           for(int i = factorial1; i > 0; i--)
+            {
+                factorial1 *= i;
+            }
+           for(int i = n-1; i >0; i--)
+            {
+                n *= i;
+            }
+           var result = factorial2/ (factorial1 * n);
+            Console.WriteLine(" Result = {0}", result);
+
+        }
+        static void CalculateTheSum()
+        {
+            Console.WriteLine("\n Write a program that for a given integers n and x, calculates the sum: "+
+                " S = 1 + 1!/x + 2!/x2+...+n!/xn ");
+            Console.WriteLine("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("x = ");
+            int x = int.Parse(Console.ReadLine());
+           int factorialN = 1;
+            int powerX = 1;
+            int sum = 0;
+
+            for(int i = n; i > 0; i--)
+            {
+                if(x > 0)
+                {
+                    factorialN *= n;
+                    powerX *= x  ;                    
+                }
+                sum += (factorialN/powerX);               
+            }
+            Console.WriteLine("Sum = {0}", sum);
+        }
+        static void PrintMatrix()
+        {
+            Console.WriteLine("\nWrite a program that reads from the console a positive integer "+
+                "number N (N < 20) and prints a matrix of numbers as on the figures below:");
+            Console.WriteLine("Input a positive integer < 20: ");
+            int numb = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+
+            for(int i = 1; i <= numb; i++)
+            {               
+                for(int j = i + 1; j <= numb +1; j ++)
+                {
+                    for(int z = j +1; z <= numb + 2; z++)
+                    {
+                        Console.WriteLine(i + " " + j + " " + z);
+                        //Console.WriteLine(z);
+                    }
+                   // Console.WriteLine(" " + j);
+
+                }
+               // Console.WriteLine(" " + " " + i);
+            }
+        }
+    }
+}
