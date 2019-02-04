5b96e6fa319509ce6b37757bec7c6faafd27d904
roxanamiron <32548532+roxanamiron@users.noreply.github.com>
2019-02-04 10:24:00 +0200

    Programing12

--- Programing1/Programing12/Program.cs
+++ Programing1/Programing12/Program.cs
@@ -11,26 +11,36 @@ namespace Programing12
         static void Main(string[] args)
         {
             //7.
-            //SquareRootOfInteger square = new SquareRootOfInteger();
-            //square.PrintSquare();
+            SquareRootOfInteger square = new SquareRootOfInteger();
+            square.PrintSquare();
 
             //8.
-            //TakeTenIntegers tenintegers = new TakeTenIntegers();
-            //tenintegers.Print();
+            TakeTenIntegers tenintegers = new TakeTenIntegers();
+            tenintegers.Print();
 
             //9.
-            //FileClass file = new FileClass();
-            //string fileName = "originalFile.txt";
-            //file.ReadFile(fileName);
+            FileClass file = new FileClass();
+            string fileName = "originalFile.txt";
+            file.ReadFile(fileName);
 
             //10.
-            //FileClass file2 = new FileClass();
-            //string fileName2 = "originalFile.txt";
-            //file2.ReturnTheFile(fileName2);
+            FileClass file2 = new FileClass();
+            string fileName2 = "originalFile.txt";
+            file2.ReturnTheFile(fileName2);
 
             //11.
+            FileParseException exception = new FileParseException("originalFile.txt", 3);
+            exception.ReadFile("originalFile.txt");
 
+            //12.
+            IOFile readFile = new IOFile();
+            Console.WriteLine("Enter the path for the existing file.");
+            string path = Console.ReadLine();
+            readFile.ReadTheFile(path);
 
+            //13.
+            DownloadAFileFromInternet download = new DownloadAFileFromInternet();
+            download.DownloadFile();
 
 
             Console.ReadLine();
